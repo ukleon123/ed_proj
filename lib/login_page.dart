e50c43414bd3cd3501963c2b3b2360e898279ed7
signo <ukleon123@gmail.com>
2023-04-13 01:30:07 +0900

    Feature login page layout/design

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,30 +1,91 @@
+import 'package:ed_proj/main_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage ({super.key});
+  const LoginPage({super.key});
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children:[
-          Image.network("https://www.kw.ac.kr/ko/img/symbol01_01.jpg"),
-
-        ]
-      ),
-      bottomNavigationBar: Row(),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [Image.asset("logo/login_logo.png"), loginForm()],
     );
   }
-  Future? login(String id, String pw) {
 
+  Future? login(String id, String pw) {
+    Map<String, String> data = {"loginId": id, "loginPwd": pw};
     return null;
+  }
+
+  Container loginForm() {
+    return Container(
+        width: MediaQuery.of(context).size.width / 1.8,
+        height: MediaQuery.of(context).size.height / 4,
+        child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextFormField(
+                  style: TextStyle(fontSize: 15),
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.grey, width: 1.0),
+                          borderRadius: BorderRadius.circular(90)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.grey, width: 2.0),
+                          borderRadius: BorderRadius.circular(90)),
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 15),
+                      hintText: 'ID'),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  style: TextStyle(fontSize: 15),
+                  decoration: InputDecoration(
+                      fillColor: Colors.grey,
+                      border: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.grey, width: 1.0),
+                          borderRadius: BorderRadius.circular(90)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.grey, width: 2.0),
+                          borderRadius: BorderRadius.circular(90)),
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 5, horizontal: 15),
+                      hintText: 'PW'),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextButton(
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => MainPage()));
+                    },
+                    child: const Text("로그인",
+                        style: TextStyle(
+                            color: Color(0xff77212c),
+                          fontSize: (17),
+                          fontWeight: FontWeight.w600
+                        )
+                    )
+                )
+              ],
+            )
+        )
+    );
   }
 }
